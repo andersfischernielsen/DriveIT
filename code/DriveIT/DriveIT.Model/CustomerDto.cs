fc2ec415706670d28ce5788d90ed694273b9ccbb
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-11-25 16:13:47 +0100

    Implemented CustomerDto.

--- code/DriveIT/DriveIT.Model/CustomerDto.cs
+++ code/DriveIT/DriveIT.Model/CustomerDto.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DriveIT.Model
+﻿namespace DriveIT.Model
 {
-    class CustomerDto
+    public class CustomerDto
     {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
     }
 }
