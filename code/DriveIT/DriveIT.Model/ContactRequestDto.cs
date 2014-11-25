d7d2720351b7d1717fa4c42816cde8f2ef4e9bdc
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-11-25 16:13:29 +0100

    Added ContactRequestDto
    Has fields about when the request was made, as well as who to contact
    about what.

---
+++ code/DriveIT/DriveIT.Model/ContactRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DriveIT.Model
+{
+    public class ContactRequestDto
+    {
+        public DateTime Requested { get; set; }
+        public int CustomerId { get; set; }
+        public int CarId { get; set; }
+    }
+}
