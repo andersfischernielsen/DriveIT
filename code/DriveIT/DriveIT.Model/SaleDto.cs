df73782906e552375116ef68df3cb60d7863f5ba
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-11-25 16:14:05 +0100

    Added SaleDto.

---
+++ code/DriveIT/DriveIT.Model/SaleDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DriveIT.Model
+{
+    public class SaleDto
+    {
+        public decimal Price { get; set; }
+        public DateTime Sold { get; set; }
+        public int CarId { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
