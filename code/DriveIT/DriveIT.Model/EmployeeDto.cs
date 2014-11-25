7ebbcf87c6a4e31f40ec7f7396b26ece1dfc41f6
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-11-25 16:13:57 +0100

    Added EmployeeDto.

---
+++ code/DriveIT/DriveIT.Model/EmployeeDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DriveIT.Model
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+    }
+}
