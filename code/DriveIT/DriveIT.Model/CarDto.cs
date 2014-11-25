72a42303ba0c9638a0dd40abd526c820e191bd92
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-11-25 15:34:30 +0100

    Initial ClassLibrary Commit
    CarDto, CommentDto almost done.
    CustomerDto is a shell.

---
+++ code/DriveIT/DriveIT.Model/CarDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DriveIT.Model
+{
+    public class CarDto
+    {
+        public long Id { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Created { get; set; }
+        public bool Sold { get; set; }
+        public float Mileage { get; set; }
+        public string Color { get; set; }
+        public int DistanceDriven { get; set; }
+        public string Fuel { get; set; }
+        public string Drive { get; set; }
+        public string Transmission { get; set; }
+    }
+}
---
+++ code/DriveIT/DriveIT.Model/CommentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DriveIT.Model
+{
+    class CommentDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public long CarId { get; set; }
+        public long CustomerId { get; set; }
+    }
+}
