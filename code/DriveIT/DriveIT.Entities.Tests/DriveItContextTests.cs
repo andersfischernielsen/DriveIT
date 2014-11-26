f8f58916c850e3f9566f8f5a6bb8bb17a619db6b
Anders Fischer-Nielsen <andersfischern@me.com>
2014-11-26 21:10:11 +0100

    Oprettede testing-projekt.

---
+++ code/DriveIT/DriveIT.Entities.Tests/DriveItContextTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DriveIT.Entities.Tests
+{
+    [TestClass]
+    public class DriveItContextTests
+    {
+        private IPersistentStorage _toTest;
+        [TestInitialize]
+        private void SetUp()
+        {
+            _toTest = new EntityAdapter();
+        }
+
+        [TestMethod]
+        public void GetAllCarsTest()
+        {
+            
+        }
+
+        public void GetCarsWithIdTest()
+        {
+
+        }
+    }
+}
