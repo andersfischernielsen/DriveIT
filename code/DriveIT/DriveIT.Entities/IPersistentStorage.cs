588a73109d62a88e1656f2e142d04dd54da596e3
Anders Fischer-Nielsen <andersfischern@me.com>
2014-11-26 21:09:50 +0100

    Oprettede Entity Framework-kobling og et Facade interface til hentning af Entities.

---
+++ code/DriveIT/DriveIT.Entities/IPersistentStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveIT.Entities
+{
+    public interface IPersistentStorage
+    {
+        Car GetCarWithId(int id);
+        IEnumerable<Car> GetAllCars();
+    }
+}
