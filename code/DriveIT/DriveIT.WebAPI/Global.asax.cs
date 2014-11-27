c7e1d2bb1a3455c19b383154808cf47ac92c60e8
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-11-27 20:32:03 +0100

    Remove Json only part of REST api.

--- code/DriveIT/DriveIT.WebAPI/Global.asax.cs
+++ code/DriveIT/DriveIT.WebAPI/Global.asax.cs
@@ -7,10 +7,6 @@ namespace DriveIT.WebAPI
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-
-            //Only return JSON from API
-            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
         }
     }
 }
