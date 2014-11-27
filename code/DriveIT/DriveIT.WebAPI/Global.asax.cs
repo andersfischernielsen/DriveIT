d2e4821a715a95cbef3251244fc26b8aa1e3ecba
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-11-27 17:13:03 +0100

    Initial API Implementation.
    CRUD Cars functionality, but doesn't work with yesterdays version of
    DriveIT.Entities.

---
+++ code/DriveIT/DriveIT.WebAPI/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace DriveIT.WebAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            //Only return JSON from API
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+        }
+    }
+}
