a30d0d0d01479f12c091da7b5e9481abf4bdde9b
Anders Fischer-Nielsen <andersfischern@me.com>
2014-11-27 14:47:34 +0100

    Oprettede CRUD-metoder i interface.

--- code/DriveIT/DriveIT.EntityFramework/EntityAdapter.cs
+++ code/DriveIT/DriveIT.EntityFramework/EntityAdapter.cs
@@ -12,7 +12,7 @@ namespace DriveIT.EntityFramework
             _context = context;
         }
 
-        public virtual Car GetCarWithId(int id)
+        public Car GetCarWithId(int id)
         {
             using (_context)
             {
@@ -20,7 +20,7 @@ namespace DriveIT.EntityFramework
             }
         }
 
-        public virtual IEnumerable<Car> GetAllCars()
+        public IEnumerable<Car> GetAllCars()
         {
             using (_context)
             {
@@ -46,6 +46,106 @@ namespace DriveIT.EntityFramework
                 await _context.SaveChangesAsync();
                 //TODO: Implement checking to see if the request happened succesfully.
             }
+        }
+
+        public Employee GetEmployeeWithId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CreateEmployee(Employee employeeToCreate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteEmployee(Employee employeeToDelete)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Customer GetCustomerWithId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CreateCustomer(Customer customerToCreate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCustomer(Customer customerToDelete)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ContactRequest GetContactRequestWithId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ContactRequest> GetAllContactRequests()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CreateContactRequest(ContactRequest contactRequestToCreate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteContactRequest(ContactRequest contactRequestToDelete)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Comment GetCommentWithId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Comment> GetAllCommentsForCar(Car car)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CreateComment(Comment commentToCreate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteComment(Comment commentToDelete)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Sale GetSaleWithId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Sale> GetAllSales()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CreateSale(Sale commentToCreate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteSale(Sale saleToDelete)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
--- code/DriveIT/DriveIT.EntityFramework/IPersistentStorage.cs
+++ code/DriveIT/DriveIT.EntityFramework/IPersistentStorage.cs
@@ -9,5 +9,30 @@ namespace DriveIT.EntityFramework
         IEnumerable<Car> GetAllCars();
         void CreateCar(Car carToCreate);
         void DeleteCar(Car carToDelete);
+
+        Employee GetEmployeeWithId(int id);
+        IEnumerable<Employee> GetAllEmployees();
+        void CreateEmployee(Employee employeeToCreate);
+        void DeleteEmployee(Employee employeeToDelete);
+
+        Customer GetCustomerWithId(int id);
+        IEnumerable<Customer> GetAllCustomers();
+        void CreateCustomer(Customer customerToCreate);
+        void DeleteCustomer(Customer customerToDelete);
+
+        ContactRequest GetContactRequestWithId(int id);
+        IEnumerable<ContactRequest> GetAllContactRequests();
+        void CreateContactRequest(ContactRequest contactRequestToCreate);
+        void DeleteContactRequest(ContactRequest contactRequestToDelete);
+
+        Comment GetCommentWithId(int id);
+        IEnumerable<Comment> GetAllCommentsForCar(Car car);
+        void CreateComment(Comment commentToCreate);
+        void DeleteComment(Comment commentToDelete);
+
+        Sale GetSaleWithId(int id);
+        IEnumerable<Sale> GetAllSales();
+        void CreateSale(Sale commentToCreate);
+        void DeleteSale(Sale saleToDelete);
     }
 }
