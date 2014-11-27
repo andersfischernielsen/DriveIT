1f6d214ac9398154b2959d9f609c23906c45c769
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-11-27 20:31:48 +0100

    Added DtoExtensions to convert CarDto to Car

---
+++ code/DriveIT/DriveIT.WebAPI/Models/DtoExtensions.cs
@@ -0,0 +1,46 @@
+﻿using DriveIT.Entities;
+using DriveIT.Models;
+
+namespace DriveIT.WebAPI.Models
+{
+    public static class DtoExtensions
+    {
+        public static Car ToCar(this CarDto dto)
+        {
+            return new Car
+            {
+                Color = dto.Color,
+                Created = dto.Created,
+                DistanceDriven = dto.DistanceDriven,
+                Fuel = dto.Fuel.ToString(),
+                Id = dto.Id,
+                Make = dto.Make,
+                Model = dto.Model,
+                Price = dto.Price,
+                Sold = dto.Sold,
+                Transmission = dto.Transmission,
+                Year = dto.Year
+            };
+        }
+
+        public static Car ToCar(this CarDetailDto dto)
+        {
+            return new Car
+            {
+                Color = dto.Color,
+                Created = dto.Created,
+                DistanceDriven = dto.DistanceDriven,
+                Fuel = dto.Fuel.ToString(),
+                Id = dto.Id,
+                Make = dto.Make,
+                Model = dto.Model,
+                Price = dto.Price,
+                Sold = dto.Sold,
+                Transmission = dto.Transmission,
+                Year = dto.Year,
+                Drive = dto.Drive,
+                Mileage = dto.Mileage
+            };
+        }
+    }
+}
