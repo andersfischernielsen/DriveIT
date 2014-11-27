5a7eb0367e8aa5a65472d8d6090b99bf78a06576
Anders Fischer-Nielsen <andersfischern@me.com>
2014-11-27 12:37:42 +0100

    Rykkede Entity Framework kommunikation over i deres eget projekt.

---
+++ code/DriveIT/DriveIT.EntityFramework/EntityAdapter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using DriveIT.Entities;
+
+namespace DriveIT.EntityFramework
+{
+    public class EntityAdapter : IPersistentStorage
+    {
+        private readonly DriveITContext _context;
+
+        public EntityAdapter(DriveITContext context)
+        {
+            _context = context;
+        }
+
+        public virtual Car GetCarWithId(int id)
+        {
+            using (_context)
+            {
+                return _context.Cars.Find(id);
+            }
+        }
+
+        public virtual IEnumerable<Car> GetAllCars()
+        {
+            using (_context)
+            {
+                return _context.Cars;
+            }
+        }
+
+        public async void CreateCar(Car carToCreate)
+        {
+            using (_context)
+            {
+                _context.Cars.Add(carToCreate);
+                await _context.SaveChangesAsync();
+                //TODO: Implement checking to see if the request happened succesfully.
+            }
+        }
+
+        public async void DeleteCar(Car carToDelete)
+        {
+            using (_context)
+            {
+                _context.Cars.Remove(carToDelete);
+                await _context.SaveChangesAsync();
+                //TODO: Implement checking to see if the request happened succesfully.
+            }
+        }
+    }
+}
---
+++ code/DriveIT/DriveIT.EntityFramework/IPersistentStorage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using DriveIT.Entities;
+
+namespace DriveIT.EntityFramework
+{
+    public interface IPersistentStorage
+    {
+        Car GetCarWithId(int id);
+        IEnumerable<Car> GetAllCars();
+        void CreateCar(Car carToCreate);
+        void DeleteCar(Car carToDelete);
+    }
+}
