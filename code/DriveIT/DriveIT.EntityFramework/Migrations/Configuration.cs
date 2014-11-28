0d4b68ec0d0bab1cbf6962e107f2a23ea813b1dd
Anders Fischer-Nielsen <andersfischern@me.com>
2014-11-28 14:10:46 +0100

    Prøvede forgæves at få migrations til at virke. It might work on your machine.

---
+++ code/DriveIT/DriveIT.EntityFramework/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace DriveIT.EntityFramework.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DriveITContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "DriveIT.EntityFramework.DriveITContext";
+        }
+
+        protected override void Seed(DriveITContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
