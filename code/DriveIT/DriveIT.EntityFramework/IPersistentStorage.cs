833bc919e79f4a56a65e55560cbb24b104af67df
Anders Fischer-Nielsen <andersfischern@me.com>
2014-11-28 11:46:35 +0100

    Ændrede kald til async.

--- code/DriveIT/DriveIT.EntityFramework/IPersistentStorage.cs
+++ code/DriveIT/DriveIT.EntityFramework/IPersistentStorage.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using DriveIT.Entities;
 
 namespace DriveIT.EntityFramework
@@ -7,37 +8,37 @@ namespace DriveIT.EntityFramework
     {
         Car GetCarWithId(int id);
         IEnumerable<Car> GetAllCars();
-        void CreateCar(Car carToCreate);
+        Task<int> CreateCar(Car carToCreate);
         void UpdateCar(int idToUpdate, Car carToReplaceWith);
         void DeleteCar(int id);
 
         Employee GetEmployeeWithId(int idToGet);
         IEnumerable<Employee> GetAllEmployees();
-        void CreateEmployee(Employee employeeToCreate);
+        Task<int> CreateEmployee(Employee employeeToCreate);
         void UpdateEmployee(int idToUpdate, Employee employeeToReplaceWith);
         void DeleteEmployee(int idToDelete);
 
         Customer GetCustomerWithId(int idToGet);
         IEnumerable<Customer> GetAllCustomers();
-        void CreateCustomer(Customer customerToCreate);
+        Task<int> CreateCustomer(Customer customerToCreate);
         void UpdateCustomer(int idToUpdate, Customer customerRequestToReplaceWith);
         void DeleteCustomer(int idToDelete);
 
         ContactRequest GetContactRequestWithId(int idToGet);
         IEnumerable<ContactRequest> GetAllContactRequests();
-        void CreateContactRequest(ContactRequest contactRequestToCreate);
+        Task<int> CreateContactRequest(ContactRequest contactRequestToCreate);
         void UpdateContactRequest(int idToUpdate, ContactRequest contactRequestToReplaceWith);
         void DeleteContactRequest(int idToDelete);
 
         Comment GetCommentWithId(int id);
         IEnumerable<Comment> GetAllCommentsForCar(Car car);
-        void CreateComment(Comment commentToCreate);
+        Task<int> CreateComment(Comment commentToCreate);
         void UpdateComment(int idToUpdate, Comment commentToReplaceWith);
         void DeleteComment(int idToDelete);
 
         Sale GetSaleWithId(int id);
         IEnumerable<Sale> GetAllSales();
-        void CreateSale(Sale commentToCreate);
+        Task<int> CreateSale(Sale commentToCreate);
         void UpdateSale(int idToUpdate, Sale saleToReplaceWith);
         void DeleteSale(Sale saleToDelete);
     }
