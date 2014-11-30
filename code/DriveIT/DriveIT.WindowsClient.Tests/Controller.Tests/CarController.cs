dd11480e9b8e889fa4996a5a3e87255d702746e0
Anders Wind Steffensen <awia00@gmail.com>
2014-11-30 21:56:20 +0100

    Small changes to a test class

---
+++ code/DriveIT/DriveIT.WindowsClient.Tests/Controller.Tests/CarController.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace DriveIT.WindowsClient.Tests.Controller.Tests
+{
+    [TestFixture]
+    public class CarController
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestAllMethods()
+        {
+        }
+    }
+}
