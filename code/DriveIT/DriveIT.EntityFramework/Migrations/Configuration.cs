cd973a000f4db0dda152ecf66ad72ceb63f6e9e3
Anders Fischer-Nielsen <andersfischern@me.com>
2014-11-30 15:01:03 +0100

    Fixed Migrations issue (maybe).

--- code/DriveIT/DriveIT.EntityFramework/Migrations/Configuration.cs
+++ code/DriveIT/DriveIT.EntityFramework/Migrations/Configuration.cs
@@ -1,32 +1,25 @@
+using DriveIT.Entities;
+
 namespace DriveIT.EntityFramework.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<DriveITContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
-            ContextKey = "DriveIT.EntityFramework.DriveITContext";
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(DriveITContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.Cars.AddOrUpdate(
+                c => c.Id, 
+                new Car
+                {
+                    Id = 1,
+                    Make = "Ford"
+                });
         }
     }
 }
