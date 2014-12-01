b85c3f90878e2e2d3e563735cd608e6056edb55e
Anders Wind Steffensen <awis@itu.dk>
2014-12-01 16:09:45 +0100

    Added a template for how to connect a usercontrol to a viewmodel

---
+++ code/DriveIT/DriveIT.WindowsClient/ViewModels/UserControlWithViewModelModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+using System.ComponentModel;
+
+namespace DriveIT.WindowsClient
+{
+	public class UserControlWithViewModelModel : INotifyPropertyChanged
+	{
+		public UserControlWithViewModelModel()
+		{
+			
+		}
+
+		#region INotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void NotifyPropertyChanged(String info)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(info));
+			}
+		}
+		#endregion
+	}
+}
