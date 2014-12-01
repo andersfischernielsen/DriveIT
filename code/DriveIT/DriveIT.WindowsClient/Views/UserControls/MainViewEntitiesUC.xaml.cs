110626be402726862a1317fb46ca78e44dbfb7d7
Anders Wind Steffensen <awis@itu.dk>
2014-12-01 16:10:02 +0100

    Added the new usercontrol which is meant to be used for the main view

---
+++ code/DriveIT/DriveIT.WindowsClient/Views/UserControls/MainViewEntitiesUC.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DriveIT.WindowsClient
+{
+	/// <summary>
+	/// Interaction logic for MainViewEntitiesUC.xaml
+	/// </summary>
+	public partial class MainViewEntitiesUC : UserControl
+	{
+		public MainViewEntitiesUC()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
