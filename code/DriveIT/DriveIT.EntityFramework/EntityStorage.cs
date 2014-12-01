dc88f428d142d4046bcf8976b31df29bca900b34
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-12-01 19:11:37 +0100

    Enabled Migrations. Added seed-method.

--- code/DriveIT/DriveIT.EntityFramework/EntityStorage.cs
+++ code/DriveIT/DriveIT.EntityFramework/EntityStorage.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Runtime.Remoting.Contexts;
 using System.Threading.Tasks;
 using DriveIT.Entities;
 
--- code/DriveIT/DriveIT.EntityFramework/Migrations/Configuration.cs
+++ code/DriveIT/DriveIT.EntityFramework/Migrations/Configuration.cs
@@ -1,3 +1,4 @@
+using System;
 using DriveIT.Entities;
 
 namespace DriveIT.EntityFramework.Migrations
@@ -8,18 +9,94 @@ namespace DriveIT.EntityFramework.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(DriveITContext context)
         {
+
+            var car = new Car
+            {
+                Id = 1,
+                Make = "Ford",
+                Model = "Focus",
+                Fuel = "Gasoline",
+                Color = "Black",
+                Created = new DateTime(2014, 12, 01),
+                DistanceDriven = 10000,
+                Drive = "FWD",
+                Mileage = 17.0f,
+                Price = 100000,
+                Sold = false,
+                Transmission = "Manual",
+                Year = 2008
+            };
+
+            var customer = new Customer
+            {
+                Id = 1,
+                Email = "cust@driveit.dk",
+                FirstName = "Cu",
+                LastName = "St",
+                Password = "Should not be present in this object",
+                PhoneNumber = "37 48 34 81",
+                Username = "cust" //Should probably not be part of this object as well.
+            };
+            var comment = new Comment
+            {
+                Id = 1,
+                CarId = 1,
+                CustomerId = 1,
+                DateCreated = new DateTime(2014, 12, 1),
+                Title = "Bad Car",
+                Description = "I think it's a bad color",
+            };
+            var employee = new Employee
+            {
+                Id = 1,
+                Email = "empl@driveit.dk",
+                FirstName = "Em",
+                LastName = "Pl",
+                Password = "Should not be present in this object",
+                PhoneNumber = "36 75 69 33",
+                Username = "empl" //Should probably no be part of this object as well.
+            };
+            var contactRequest = new ContactRequest
+            {
+                Id = 1,
+                CarId = 1,
+                CustomerId = 1,
+                Created = new DateTime(2014, 12, 1),
+                EmployeeId = 1
+            };
+            var sale = new Sale
+            {
+                Id = 1,
+                CarId = 1,
+                CustomerId = 1,
+                EmployeeId = 1,
+                Price = 1000,
+                DateOfSale = new DateTime(2014, 12, 1)
+            };
+
             context.Cars.AddOrUpdate(
-                c => c.Id, 
-                new Car
-                {
-                    Id = 1,
-                    Make = "Ford"
-                });
+                c => c.Id,
+                car);
+            context.Customers.AddOrUpdate(
+                c => c.Id,
+                customer);
+            context.Comments.AddOrUpdate(
+                c => c.Id,
+                comment);
+            context.Employees.AddOrUpdate(
+                e => e.Id,
+                employee);
+            context.ContactRequests.AddOrUpdate(
+                c => c.Id,
+                contactRequest);
+            context.Sales.AddOrUpdate(
+                s => s.Id,
+                sale);
         }
     }
 }
