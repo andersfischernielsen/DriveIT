806158bb5d6e585e1c2d82beeadf1890dc8da8b7
Pierre Popino Mandas <ppma@itu.dk>
2014-12-01 23:06:59 +0100

    Committing CarController and CarSearch view

---
+++ code/DriveIT/DriveIT.MVC/Controllers/CarController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using DriveIT.Models;
+
+namespace DriveIT.MVC.Controllers
+{
+    public class CarController : AsyncController
+    {
+
+        // GET: Car/CarSearch
+        public async Task<ActionResult> CarSearchAsync()
+        {
+            return View();
+        }
+
+        public async void CreateCar(CarDto car)
+        {
+            await DriveITWebAPI.Create("cars", car);
+        }
+
+        public async Task<IList<CarDto>> GetCarsByFuelType(string fuelType)
+        {
+            var carsToReturn = await DriveITWebAPI.ReadList<CarDto>("Cars?fuelType=" + fuelType);
+            return carsToReturn;
+        }
+
+        public async Task<IList<CarDto>> GetCarsByMake(string make)
+        {
+            var carsToReturn = await DriveITWebAPI.ReadList<CarDto>("Cars?make=" + make);
+            return carsToReturn;
+        }
+
+        public async Task<IList<CarDto>> GetCarsByModel(string model)
+        {
+            var carsToReturn = await DriveITWebAPI.ReadList<CarDto>("Cars?model=" + model);
+            return carsToReturn;
+        }
+
+        public async Task<IList<CarDto>> GetCarsByMakeAndModel(string make, string model)
+        {
+            var carsToReturn = await DriveITWebAPI.ReadList<CarDto>("Cars?make=" + make + "&model=" + model);
+            return carsToReturn;
+        }
+    }
+}
