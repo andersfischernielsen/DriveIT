5148b61c26387e493b14b0714911b2b60cbaa414
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-12-01 19:11:11 +0100

    Added Attributes + Foreign Ids

--- code/DriveIT/DriveIT.Entities/Employee.cs
+++ code/DriveIT/DriveIT.Entities/Employee.cs
@@ -1,6 +1,16 @@
-﻿namespace DriveIT.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DriveIT.Entities
 {
-    public class Employee : Customer
+    public class Employee
     {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
     }
 }
