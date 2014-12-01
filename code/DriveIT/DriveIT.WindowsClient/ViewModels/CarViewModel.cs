13905342b7d40b01c71f0825d2a5fc20f434a331
Christopher Blundell <christopher.n.blundell@gmail.com>
2014-12-02 00:09:37 +0100

    Fixed GetCarID and added all methods for retrieving attributes.

--- code/DriveIT/DriveIT.WindowsClient/ViewModels/CarViewModel.cs
+++ code/DriveIT/DriveIT.WindowsClient/ViewModels/CarViewModel.cs
@@ -10,11 +10,20 @@ namespace DriveIT.WindowsClient.ViewModels
     {
         private CarDto _carDto;
 
-        public int CarId
+        public int? CarId
         {
             get
             {
-                return _carDto.Id.GetValueOrDefault();
+                try
+                {
+                    return _carDto.Id.Value;
+                }
+                catch (Exception)
+                {
+
+                    return null;
+                }
+                
             }
             set
             {
@@ -22,6 +31,18 @@ namespace DriveIT.WindowsClient.ViewModels
                 NotifyPropertyChanged("CarId");
             }
         }
+
+
+        public CarViewModel(CarDto carDto)
+        {
+            _carDto = carDto;
+        }
+        public CarViewModel()
+        {
+            
+        }
+
+        #region Attributes
         public string CarModel
         {
             get
@@ -47,15 +68,151 @@ namespace DriveIT.WindowsClient.ViewModels
             }
         }
 
-        
-        public CarViewModel(CarDto carDto)
+        public int Year
         {
-            _carDto = carDto;
+            get
+            {
+                return _carDto.Year;
+            }
+            set
+            {
+                _carDto.Year = value;
+                NotifyPropertyChanged("Year");
+            }
         }
-        public CarViewModel()
+        public decimal Price
         {
-            
+            get
+            {
+                return _carDto.Price;
+            }
+            set
+            {
+                _carDto.Price = value;
+                NotifyPropertyChanged("Price");
+            }
         }
+        public DateTime Created
+        {
+            get
+            {
+                return _carDto.Created;
+            }
+            set
+            {
+                _carDto.Created = value;
+                NotifyPropertyChanged("Created");
+            }
+        }
+        public bool Sold
+        {
+            get
+            {
+                return _carDto.Sold;
+            }
+            set
+            {
+                _carDto.Sold = value;
+                NotifyPropertyChanged("Sold");
+            }
+        }
+        public float Mileage
+        {
+            get
+            {
+                return _carDto.Mileage;
+            }
+            set
+            {
+                _carDto.Mileage = value;
+                NotifyPropertyChanged("Mileage");
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                return _carDto.Color;
+            }
+            set
+            {
+                _carDto.Color = value;
+                NotifyPropertyChanged("Color");
+            }
+        }
+        public int DistanceDriven
+        {
+            get
+            {
+                return _carDto.DistanceDriven;
+            }
+            set
+            {
+                _carDto.DistanceDriven = value;
+                NotifyPropertyChanged("DistanceDriven");
+            }
+        }
+        public FuelType Fuel
+        {
+            get
+            {
+                return _carDto.Fuel;
+            }
+            set
+            {
+                _carDto.Fuel = value;
+                NotifyPropertyChanged("Fuel");
+            }
+        }
+        public string Drive
+        {
+            get
+            {
+                return _carDto.Drive;
+            }
+            set
+            {
+                _carDto.Drive = value;
+                NotifyPropertyChanged("Drive");
+            }
+        }
+        public string Transmission
+        {
+            get
+            {
+                return _carDto.Transmission;
+            }
+            set
+            {
+                _carDto.Transmission = value;
+                NotifyPropertyChanged("Transmission");
+            }
+        }
+        public int TopSpeed
+        {
+            get
+            {
+                return _carDto.TopSpeed;
+            }
+            set
+            {
+                _carDto.TopSpeed = value;
+                NotifyPropertyChanged("TopSpeed");
+            }
+        }
+        public int NoughtTo100
+        {
+            get
+            {
+                return _carDto.NoughtTo100;
+            }
+            set
+            {
+                _carDto.NoughtTo100 = value;
+                NotifyPropertyChanged("NoughtTo100");
+            }
+        }
+        #endregion Attributes
 
         #region CRUDS
         /// <summary>
