150cf4145c6f1ec5fb149a87ea3adb627db03786
Christopher Blundell <christopher.n.blundell@gmail.com>
2014-12-02 15:38:20 +0100

    Created DTO's for Customer and CustomerList.

--- code/DriveIT/DriveIT.WindowsClient/ViewModels/CustomerListViewModel.cs
+++ code/DriveIT/DriveIT.WindowsClient/ViewModels/CustomerListViewModel.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DriveIT.Models;
+using DriveIT.WindowsClient.ViewModels;
+using DriveIT_Windows_Client.Controllers;
 
 namespace DriveIT_Windows_Client.ViewModels
 {
     public class CustomerListViewModel : IViewModelBase
     {
+        public ObservableCollection<CustomerViewModel> CustomerViewModels { get; set; }
+
+        public CustomerListViewModel(IList<CustomerDto> customerDtos)
+        {
+            CustomerViewModels = new ObservableCollection<CustomerViewModel>();
+            foreach (CustomerDto customerDto in customerDtos)
+            {
+                CustomerViewModels.Add(new CustomerViewModel(customerDto));
+            }
+        }
+
+        public CustomerListViewModel()
+        {
+            CustomerViewModels = new ObservableCollection<CustomerViewModel>();
+            ReadList();
+        }
+
+        #region CRUD
+
+        public async void ReadList()
+        {
+            var carController = new CarController();
+            foreach (CarDto carDto in await carController.ReadCarList())
+            {
+                CustomerViewModels.Add(new CarViewModel(carDto));
+            }
+        }
+
+        #endregion CRUD
+
         #region INotifyPropertyChanged
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void NotifyPropertyChanged(String info)
@@ -19,6 +54,7 @@ namespace DriveIT_Windows_Client.ViewModels
                 PropertyChanged(this, new PropertyChangedEventArgs(info));
             }
         }
+
         #endregion
     }
 }
--- code/DriveIT/DriveIT.WindowsClient/ViewModels/CustomerViewModel.cs
+++ code/DriveIT/DriveIT.WindowsClient/ViewModels/CustomerViewModel.cs
@@ -4,11 +4,136 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DriveIT.Models;
+using DriveIT_Windows_Client.Controllers;
 
 namespace DriveIT_Windows_Client.ViewModels
 {
     public class CustomerViewModel : IViewModelBase
     {
+        private CustomerDto _customerDto;
+
+        public int? Id
+        {
+            get
+            {
+                try
+                {
+                    return _customerDto.Id.Value;
+                }
+                catch (Exception)
+                {
+
+                    return null;
+                }
+
+            }
+            set
+            {
+                _customerDto.Id = value;
+                NotifyPropertyChanged("Id");
+            }
+        }
+
+        public CustomerViewModel(CustomerDto customerDto)
+        {
+            _customerDto = customerDto;
+        }
+        public CustomerViewModel()
+        {
+            
+        }
+
+        #region ATTRIBUTES
+        public string Username
+        {
+            get
+            {
+                return _customerDto.Username;
+            }
+            set
+            {
+                _customerDto.Username = value;
+                NotifyPropertyChanged("Username");
+            }
+        }
+        public string FirstName
+        {
+            get
+            {
+                return _customerDto.FirstName;
+            }
+            set
+            {
+                _customerDto.FirstName = value;
+                NotifyPropertyChanged("FirstName");
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _customerDto.LastName;
+            }
+            set
+            {
+                _customerDto.LastName = value;
+                NotifyPropertyChanged("LastName");
+            }
+        }
+        public string Phone
+        {
+            get
+            {
+                return _customerDto.Phone;
+            }
+            set
+            {
+                _customerDto.Phone = value;
+                NotifyPropertyChanged("Phone");
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return _customerDto.Email;
+            }
+            set
+            {
+                _customerDto.Email = value;
+                NotifyPropertyChanged("Email");
+            }
+        }
+        #endregion ATTRIBUTES
+
+        #region CRUDS
+        /// <summary>
+        /// Gets called from the view
+        /// </summary>
+        public void CreateCustomer()
+        {
+            var customerController = new CustomerController();
+            customerController.CreateCustomer(_customerDto);
+        }
+        /// <summary>
+        /// Gets called from the view
+        /// </summary>
+        public void UpdateCustomer()
+        {
+            var customerController = new CustomerController();
+            customerController.UpdateCustomer(_customerDto);
+        }
+        /// <summary>
+        /// Gets called from the view
+        /// </summary>
+        public void DeleteCustomer()
+        {
+            var customerController = new CustomerController();
+            customerController.DeleteCustomer(_customerDto);
+        }
+        #endregion CRUDS
+
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
 
