7d9172f6f2b2616a1f196fe6af8ba97757ae6712
Anders Fischer-Nielsen <andersfischern@me.com>
2014-12-02 15:30:46 +0100

    Skiftede til NUnit.

--- code/DriveIT/DriveIT.CarQuery.Tests/CarQueryTests.cs
+++ code/DriveIT/DriveIT.CarQuery.Tests/CarQueryTests.cs
@@ -1,15 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using DriveIT.CarQuery;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit;
+using NUnit.Framework;
 
 namespace DriveIT.CarQuery.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class CarQueryTests
     {
-        [TestInitialize]
+        [TestFixtureSetUp]
         public void SetUp()
         {
 
+        }
+
+        [Test]
+        public async void TestRead()
+        {
+            var result = await JSONWrapper.ReadList<Trim>("make=ford");
+
+            foreach (var car in result)
+            {
+                Console.WriteLine(car.model_body + " & " + car.model_doors);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            
         }
     }
 }
