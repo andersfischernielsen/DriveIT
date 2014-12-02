90ffd9f69fbb32737159b61c55dd425094d9ca65
Anders Wind Steffensen <awis@itu.dk>
2014-12-02 14:14:27 +0100

    Integration test from WPF works

--- code/DriveIT/DriveIT.WindowsClient.Tests/Controller.Tests/EmployeeControllerTests.cs
+++ code/DriveIT/DriveIT.WindowsClient.Tests/Controller.Tests/EmployeeControllerTests.cs
@@ -41,14 +41,15 @@ namespace DriveIT.WindowsClient.Tests.Controller.Tests
             {
                 Username = "sexydude123",
                 FirstName = "Mr Not So Handsome",
-                LastName = "Cookie"
+                LastName = "Cookie",
+                Id = t[t.Count - 1].Id.Value
             });
             Thread.Sleep(2000);
             t = _employeeController.ReadEmployeeList().Result;
             Console.WriteLine(t.Count);
             Console.WriteLine("After update: " + _employeeController.ReadEmployee(t[t.Count - 1].Id.Value).Result.FirstName);
 
-            _employeeController.DeleteEmployee(t[0].Id.Value);
+            _employeeController.DeleteEmployee(t[t.Count - 1].Id.Value);
             Thread.Sleep(2000);
             t = _employeeController.ReadEmployeeList().Result;
             Console.WriteLine(t.Count);
