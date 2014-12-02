497bb99608cca6ee052f82c8c2c4a29b9e6e9453
Anders Fischer-Nielsen <andersfischern@me.com>
2014-12-02 14:06:46 +0100

    Initial commit.

---
+++ code/DriveIT/DriveIT.CarQuery.Tests/CarQueryTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DriveIT.CarQuery.Tests
+{
+    [TestClass]
+    public class CarQueryTests
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+
+        }
+    }
+}
