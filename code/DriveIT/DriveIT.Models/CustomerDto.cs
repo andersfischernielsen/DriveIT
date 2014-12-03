a97b633772ddcc39d3a4affe67a5344c541c764a
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-12-03 11:22:03 +0100

    Username and password removed from entities.

--- code/DriveIT/DriveIT.Models/CustomerDto.cs
+++ code/DriveIT/DriveIT.Models/CustomerDto.cs
@@ -3,7 +3,6 @@
     public class CustomerDto
     {
         public int? Id { get; set; }
-        public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Phone { get; set; }
--- code/DriveIT/DriveIT.Models/EmployeeDto.cs
+++ code/DriveIT/DriveIT.Models/EmployeeDto.cs
@@ -3,7 +3,7 @@
     public class EmployeeDto
     {
         public int? Id { get; set; }
-        public string Username { get; set; }
+        public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Phone { get; set; }
