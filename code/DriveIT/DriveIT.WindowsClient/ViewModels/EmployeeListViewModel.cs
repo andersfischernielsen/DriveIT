f1e6ab2a765056e653d3e359a435b9d92a5f3e20
Christopher Blundell <christopher.n.blundell@gmail.com>
2014-12-03 10:56:21 +0100

    Created DTO for EmployeeList.

--- code/DriveIT/DriveIT.WindowsClient/ViewModels/EmployeeListViewModel.cs
+++ code/DriveIT/DriveIT.WindowsClient/ViewModels/EmployeeListViewModel.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using DriveIT.Models;
+using DriveIT.WindowsClient.Controllers;
 
 namespace DriveIT.WindowsClient.ViewModels
 {
     public class EmployeeListViewModel : IViewModelBase
     {
+    public ObservableCollection<EmployeeViewModel> EmployeeViewModels { get; set; }
+
+        public EmployeeListViewModel(IList<EmployeeDto> employeeDtos)
+        {
+            EmployeeViewModels = new ObservableCollection<EmployeeViewModel>();
+            foreach (EmployeeDto employeeDto in employeeDtos)
+            {
+                EmployeeViewModels.Add(new EmployeeViewModel(employeeDto));
+            }
+        }
+        public EmployeeListViewModel()
+        {
+            EmployeeViewModels = new ObservableCollection<EmployeeViewModel>();
+            ReadList();
+        }
+ 
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -16,5 +36,16 @@ namespace DriveIT.WindowsClient.ViewModels
             }
         }
         #endregion
+
+        #region CRUD
+        public async void ReadList()
+        {
+            var employeeController = new EmployeeController();
+            foreach (EmployeeDto employeeDto in await employeeController.ReadEmployeeList())
+            {
+                EmployeeViewModels.Add(new EmployeeViewModel(employeeDto));
+            }
+        }
+        #endregion CRUD
     }
 }
