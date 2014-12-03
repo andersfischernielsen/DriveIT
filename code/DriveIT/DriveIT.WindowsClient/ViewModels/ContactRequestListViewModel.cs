948e3c6a97c217e1f26f6eeb3593f5cf331f5a79
Christopher Blundell <christopher.n.blundell@gmail.com>
2014-12-03 11:51:48 +0100

    Created DTO for ContactRequestList.

--- code/DriveIT/DriveIT.WindowsClient/ViewModels/ContactRequestListViewModel.cs
+++ code/DriveIT/DriveIT.WindowsClient/ViewModels/ContactRequestListViewModel.cs
@@ -1,11 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using DriveIT.Models;
+using DriveIT.WindowsClient.Controllers;
 
 namespace DriveIT.WindowsClient.ViewModels
 {
     public class ContactRequestListViewModel : IViewModelBase
     {
+        public ObservableCollection<ContactRequestViewModel> ContactRequestViewModels { get; set; }
+
+        public ContactRequestListViewModel(IList<ContactRequestDto> contactRequestDtos)
+        {
+            ContactRequestViewModels = new ObservableCollection<ContactRequestViewModel>();
+            foreach (ContactRequestDto contactRequestDto in contactRequestDtos)
+            {
+                ContactRequestViewModels.Add(new ContactRequestViewModel(contactRequestDto));
+            }
+        }
+        public ContactRequestListViewModel()
+        {
+            ContactRequestViewModels = new ObservableCollection<ContactRequestViewModel>();
+            ReadList();
+        }
+
+        #region CRUD
+
+        public async void ReadList()
+        {
+            var contactRequestController = new ContactRequestController();
+            foreach (ContactRequestDto contactRequestDto in await contactRequestController.ReadContactRequests())
+            {
+                ContactRequestViewModels.Add(new ContactRequestViewModel(contactRequestDto));
+            }
+        }
+
+        #endregion CRUD
+
         #region INotifyPropertyChanged
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void NotifyPropertyChanged(String info)
@@ -15,6 +49,7 @@ namespace DriveIT.WindowsClient.ViewModels
                 PropertyChanged(this, new PropertyChangedEventArgs(info));
             }
         }
+
         #endregion
     }
 }
