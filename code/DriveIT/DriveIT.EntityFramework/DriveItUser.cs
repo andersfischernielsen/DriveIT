a85e2ef5633a2fb42fbb3fc92cf6514431684a24
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-12-03 18:13:17 +0100

    Add fields to DriveItUser
    Still need to map to Entities.

--- code/DriveIT/DriveIT.EntityFramework/DriveItUser.cs
+++ code/DriveIT/DriveIT.EntityFramework/DriveItUser.cs
@@ -1,13 +1,49 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using DriveIT.Entities;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace DriveIT.EntityFramework
 {
-// ReSharper disable once InconsistentNaming
+    // ReSharper disable once InconsistentNaming
     public class DriveITUser : IdentityUser
     {
+        private int _customerId;
+        private int _employeeId;
+
+        public int CustomerId
+        {
+            get { return _customerId; }
+            set
+            {
+                if (IsEmployee()) throw new InvalidOperationException("Can't be both customer and employee");
+                _customerId = value;
+            }
+        }
+        public virtual Customer Customer { get; set; }
+        public int EmployeeId
+        {
+            get { return _employeeId; }
+            set
+            {
+                if (IsCustomer()) throw new InvalidOperationException("Can't be both customer and employee");
+                _customerId = value;
+            }
+        }
+        public virtual Employee Employee { get; set; }
+
+        public bool IsCustomer()
+        {
+            return CustomerId > 0;
+        }
+
+        public bool IsEmployee()
+        {
+            return EmployeeId > 0;
+        }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<DriveITUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
