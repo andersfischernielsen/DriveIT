38cfb5324175af5c87367118a08ee215120ba947
Christopher Blundell <christopher.n.blundell@gmail.com>
2014-12-03 11:20:55 +0100

    Renamed "Order" controllers/viewmodels to "Sale". Also, created DTO for both Sale and SaleList

---
+++ code/DriveIT/DriveIT.WindowsClient/ViewModels/SaleListViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using DriveIT.Models;
+using DriveIT.WindowsClient.Controllers;
+
+namespace DriveIT.WindowsClient.ViewModels
+{
+    public class SaleListViewModel : IViewModelBase
+    {
+    public ObservableCollection<SaleViewModel> SaleViewModels { get; set; }
+
+        public SaleListViewModel(IList<SaleDto> saleDtos)
+        {
+            SaleViewModels = new ObservableCollection<SaleViewModel>();
+            foreach (SaleDto saleDto in saleDtos)
+            {
+                SaleViewModels.Add(new SaleViewModel(saleDto));
+            }
+        }
+        public SaleListViewModel()
+        {
+            SaleViewModels = new ObservableCollection<SaleViewModel>();
+            ReadList();
+        }
+ 
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+        #endregion
+
+        #region CRUD
+        public async void ReadList()
+        {
+            var saleController = new SaleController();
+            foreach (SaleDto saleDto in await saleController.ReadSaleList())
+            {
+                SaleViewModels.Add(new SaleViewModel(saleDto));
+            }
+        }
+        #endregion CRUD
+    }
+}
