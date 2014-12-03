07ea3464fe475637ffa743160f43360248562771
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-12-03 11:20:30 +0100

    Username and password removed from entities.

--- code/DriveIT/DriveIT.Entities/Customer.cs
+++ code/DriveIT/DriveIT.Entities/Customer.cs
@@ -6,8 +6,6 @@ namespace DriveIT.Entities
     {
         [Key]
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
