28c44d55f4ca65a8696c1a84adf8a19765ccb6f7
Anders Fischer-Nielsen <andersfischern@me.com>
2014-12-04 11:55:10 +0100

    Made code a bit more readable.

--- code/DriveIT/DriveIT.CarQuery/JSONWrapper.cs
+++ code/DriveIT/DriveIT.CarQuery/JSONWrapper.cs
@@ -11,20 +11,7 @@ namespace DriveIT.CarQuery
     public static class JSONWrapper
     {
         private const string Uri = @"http://www.carqueryapi.com/api/0.3/";
-        private const string Parameter = "?&cmd=getTrims&";
-
-        private static void SetUpHttpClient(HttpClient client)
-        {
-            client.BaseAddress = new Uri(Uri);
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        }
-
-        private static async Task<HttpResponseMessage> GetAndEnsureResponse(string request, HttpClient client)
-        {
-            var response = await client.GetAsync(Parameter + request);
-            response.EnsureSuccessStatusCode();
-            return response;
-        }
+        private const string GetTrimsParameter = "?&cmd=getTrims&";
 
         public static async Task<T> Read<T>(string request)
         {
@@ -36,15 +23,27 @@ namespace DriveIT.CarQuery
                 {
                     SetUpHttpClient(httpClient);
                     var response = await GetAndEnsureResponse(request, httpClient);
-
                     received = await response.Content.ReadAsAsync<T>();
                 }
                 catch (Exception)
                 {
-                    throw;
+                    received = default(T);
                 }
             }
             return received;
+        }
+
+        private static void SetUpHttpClient(HttpClient client)
+        {
+            client.BaseAddress = new Uri(Uri);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        private static async Task<HttpResponseMessage> GetAndEnsureResponse(string request, HttpClient client)
+        {
+            var response = await client.GetAsync(GetTrimsParameter + request);
+            response.EnsureSuccessStatusCode();
+            return response;
         }
     }
 }
