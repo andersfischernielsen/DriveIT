3dd59dd7f7995c8789de37b7933cdcf2e52ddb36
Anders Fischer-Nielsen <andersfischern@me.com>
2014-12-04 11:03:08 +0100

    Fixed if statements. Some code was unreachable. Added functionality for finding more specific car info.

--- code/DriveIT/DriveIT.CarQuery/CarQuery.cs
+++ code/DriveIT/DriveIT.CarQuery/CarQuery.cs
@@ -21,30 +21,37 @@ namespace DriveIT.CarQuery
             if (string.IsNullOrEmpty(model) || string.IsNullOrWhiteSpace(model))
                 throw new Exception("Model of the CarDto object must not be null.");
 
+            var query = "make=" + make + "&model=" + model;
+
+            if (carToFill.Year.HasValue)
+            {
+                query += "&year=" + carToFill.Year.Value;
+            }
+
             TrimArray result = null;
             if (carToFill.Drive == null)
             {
-                result = await JSONWrapper.Read<TrimArray>("make=" + make + "&" + "model=" + model);
+                result = await JSONWrapper.Read<TrimArray>(query);
                 carToFill.Drive = result.Trims[0].model_drive;
             }
 
-            if (carToFill.NoughtTo100 == 0 && result == null)
+            if (carToFill.NoughtTo100 == 0)
             {
-                result = await JSONWrapper.Read<TrimArray>("make=" + make + "&" + "model=" + model);
+                if (result == null) result = await JSONWrapper.Read<TrimArray>(query);
                 if (result.Trims[0].model_0_to_100_kph.HasValue) 
                     carToFill.NoughtTo100 = result.Trims[0].model_0_to_100_kph.Value;
             }
 
-            if (carToFill.TopSpeed == 0 && result == null)
+            if (carToFill.TopSpeed == 0)
             {
-                result = await JSONWrapper.Read<TrimArray>("make=" + make + "&" + "model=" + model);
+                if (result == null) result = await JSONWrapper.Read<TrimArray>(query);
                 if (result.Trims[0].model_top_speed_kph.HasValue) 
                     carToFill.TopSpeed = result.Trims[0].model_top_speed_kph.Value;
             }
 
-            if ((string.IsNullOrEmpty(carToFill.Transmission) || carToFill.Transmission.Equals(" ")) && result == null)
+            if (string.IsNullOrEmpty(carToFill.Transmission) || string.IsNullOrWhiteSpace(carToFill.Transmission))
             {
-                result = await JSONWrapper.Read<TrimArray>("make=" + make + "&" + "model=" + model);
+                if (result == null) result = await JSONWrapper.Read<TrimArray>(query);
                 carToFill.Transmission = result.Trims[0].model_transmission_type;
             }
 
