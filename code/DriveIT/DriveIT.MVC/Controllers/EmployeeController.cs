51bc3a76a371357c18b092af50f13627091abbd5
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2014-12-05 10:58:48 +0100

    started making employees

--- code/DriveIT/DriveIT.MVC/Controllers/EmployeeController.cs
+++ code/DriveIT/DriveIT.MVC/Controllers/EmployeeController.cs
@@ -5,15 +5,18 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using DriveIT.Models;
+using DriveIT.WebAPI.Controllers;
 
 namespace DriveIT.MVC.Controllers
 {
     public class EmployeeController : AsyncController
     {
+        private EmployeesController controller = new EmployeesController();
         // GET: Employee
-        public ActionResult EmployeeView()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            //var emps = await controller.Get() as OkNegotiatedContentResult<List<EmployeeDto>>;
+            //return View(emps);
         }
 
         public async Task<IList<EmployeeDto>> ReadEmployeeList()
