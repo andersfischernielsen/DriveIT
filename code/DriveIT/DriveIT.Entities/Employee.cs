0534dfe775a1c59024c1ddeb5c5a1896a745c9b6
Mikael Jepsen <mikaellindemannjepsen@gmail.com>
2014-12-05 12:21:25 +0100

    Add user entity and Roles.
    Customer and Employee inherits DriveIT User.

--- code/DriveIT/DriveIT.Entities/Employee.cs
+++ code/DriveIT/DriveIT.Entities/Employee.cs
@@ -2,13 +2,7 @@
 
 namespace DriveIT.Entities
 {
-    public class Employee
+    public class Employee : DriveITUser
     {
-        [Key]
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
     }
 }
