ab90daf887a12550609368d620c7bfe888627e3b
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2014-12-05 11:55:53 +0100

    changed contact controller in web api to make dto to list

--- code/DriveIT/DriveIT.MVC/Controllers/EmployeeController.cs
+++ code/DriveIT/DriveIT.MVC/Controllers/EmployeeController.cs
@@ -13,16 +13,10 @@ namespace DriveIT.MVC.Controllers
     {
         private EmployeesController controller = new EmployeesController();
         // GET: Employee
-        public async Task<ActionResult> Index()
-        {
-            //var emps = await controller.Get() as OkNegotiatedContentResult<List<EmployeeDto>>;
-            //return View(emps);
-        }
-
-        public async Task<IList<EmployeeDto>> ReadEmployeeList()
-        {
-            var employees = await DriveITWebAPI.ReadList<EmployeeDto>("Employees");
-            return employees;
-        }
+        //public async Task<ActionResult> Index()
+        //{
+        //    //var emps = await controller.Get() as OkNegotiatedContentResult<List<EmployeeDto>>;
+        //    //return View(emps);
+        //}
     }
 }
