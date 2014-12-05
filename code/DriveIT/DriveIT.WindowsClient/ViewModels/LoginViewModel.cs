cbe60cfcf9aab606e8bd99eaf69fd775528ad9ed
Anders Wind Steffensen <awia00@gmail.com>
2014-12-05 21:28:01 +0100

    Created functionality to login to the system via WPF (when merging with master)

---
+++ code/DriveIT/DriveIT.WindowsClient/ViewModels/LoginViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DriveIT.WindowsClient.Controllers;
+
+namespace DriveIT.WindowsClient.ViewModels
+{
+    public class LoginViewModel
+    {
+        private string _username;
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                NotifyPropertyChanged("Username");
+            }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyPropertyChanged("Password");
+            }
+        }
+
+        private string _status;
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+                NotifyPropertyChanged("Status");
+            }
+        }
+
+
+        public async void Login()
+        {
+            try
+            {
+                await DriveITWebAPI.Login(Username, Password);
+            }
+            catch (Exception)
+            {
+                Status = "Username or password is invalid.";
+                Password = null;
+                Username = null;
+                throw;
+            }
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+        #endregion
+    }
+}
