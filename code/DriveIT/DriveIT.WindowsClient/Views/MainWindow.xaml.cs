2c394490f13c75533bb0018cc88df0dbc5a00ffb
Anders Wind Steffensen <awia00@gmail.com>
2014-12-07 23:28:21 +0100

    Fixed the last none MVVM Pattern controls to a MVVM style

--- code/DriveIT/DriveIT.WindowsClient/Views/MainWindow.xaml.cs
+++ code/DriveIT/DriveIT.WindowsClient/Views/MainWindow.xaml.cs
@@ -29,41 +29,5 @@ namespace DriveIT.WindowsClient.Views
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
             e.Handled = true;
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var entityWindow = new EntityCarWindow();
-            entityWindow.Show(); // works
-        }
-
-        private void RequestsForContactsClick(object sender, RoutedEventArgs e)
-        {
-            EntitiesTabControl.SelectedIndex = 0;
-            PowerToolsbarTabControl.SelectedIndex = 0;
-        }
-
-        private void CarsClick(object sender, RoutedEventArgs e)
-        {
-            EntitiesTabControl.SelectedIndex = 1;
-            PowerToolsbarTabControl.SelectedIndex = 1;
-        }
-
-        private void OrdersClick(object sender, RoutedEventArgs e)
-        {
-            EntitiesTabControl.SelectedIndex = 2;
-            PowerToolsbarTabControl.SelectedIndex = 2;
-        }
-
-        private void CustomersClick(object sender, RoutedEventArgs e)
-        {
-            EntitiesTabControl.SelectedIndex = 3;
-            PowerToolsbarTabControl.SelectedIndex = 3;
-        }
-
-        private void EmployeesClick(object sender, RoutedEventArgs e)
-        {
-            EntitiesTabControl.SelectedIndex = 4;
-            PowerToolsbarTabControl.SelectedIndex = 4;
-        }
     }
 }
