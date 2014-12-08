34b5f6a75b23dfcadc648be37b86384d35d5f9bf
Anders Wind Steffensen <awia00@gmail.com>
2014-12-08 21:52:24 +0100

    fixed runtime error

--- code/DriveIT/DriveIT.WindowsClient/ViewModels/CarViewModel.cs
+++ code/DriveIT/DriveIT.WindowsClient/ViewModels/CarViewModel.cs
@@ -40,7 +40,7 @@ namespace DriveIT.WindowsClient.ViewModels
         {
             _carDto = carDto;
             CarState = CarStateEnum.ForSale;
-            ImageViewModel = new ImageViewModel(_carDto.ImagePaths[0]);
+            if (_carDto.ImagePaths != null) ImageViewModel = new ImageViewModel(_carDto.ImagePaths[0]);
         }
         public CarViewModel()
         {
