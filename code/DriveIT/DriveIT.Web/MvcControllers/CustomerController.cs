92c206c29fbc306e9ccd16e0d10c3b22cbc5527d
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2014-12-10 18:32:58 +0100

    finished basic customer and employee

--- code/DriveIT/DriveIT.Web/MvcControllers/CustomerController.cs
+++ code/DriveIT/DriveIT.Web/MvcControllers/CustomerController.cs
@@ -10,13 +10,6 @@ namespace DriveIT.Web.MvcControllers
     public class CustomerController : AsyncController
     {
         private CustomersController controller = new CustomersController();
-        
-        //GET: Customer
-        public async Task<ActionResult> Index()
-        {
-            var customers = await controller.Get() as OkNegotiatedContentResult<List<CustomerDto>>;
-            return View(customers.Content);
-        }
 
         public async Task<ActionResult> Show(string id)
         {
@@ -24,39 +17,26 @@ namespace DriveIT.Web.MvcControllers
             return View(customer.Content);
         }
 
-        [System.Web.Mvc.HttpGet]
-        public ActionResult Create()
-        {
-            return View();
-        }
-        
-        //[System.Web.Mvc.HttpPost]
-        //public async Task<ActionResult> Create(CustomerDto cust)
-        //{
-        //    var customer = await controller.Post(cust) as CreatedAtRouteNegotiatedContentResult<CustomerDto>;
-        //    return RedirectToAction("Show", customer.Content);
-        //}
-
-        [System.Web.Mvc.HttpGet]
+        [HttpGet]
         public async Task<ActionResult> Edit(string id)
         {
             var customer = await controller.Get(id) as OkNegotiatedContentResult<CustomerDto>;
             return View(customer.Content);
         }
 
-        [System.Web.Mvc.HttpPost]
+        [HttpPost]
         public async Task<ActionResult> Edit(CustomerDto cust)
         {
             await controller.Put(cust.Id, cust);
             // Customer to redirect to
             var customer = await controller.Get(cust.Id) as OkNegotiatedContentResult<CustomerDto>;
-            return RedirectToAction("Show", customer.Content);
+            return RedirectToAction("Index", "Manage");
         }
 
         public async Task<ActionResult> Delete(string id)
         {
             await controller.Delete(id);
-            return RedirectToAction("Index");
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- code/DriveIT/DriveIT.Web/MvcControllers/EmployeeController.cs
+++ code/DriveIT/DriveIT.Web/MvcControllers/EmployeeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+using DriveIT.Models;
 using DriveIT.Web.ApiControllers;
 
 namespace DriveIT.Web.MvcControllers
@@ -6,11 +10,19 @@ namespace DriveIT.Web.MvcControllers
     public class EmployeeController : AsyncController
     {
         private EmployeesController controller = new EmployeesController();
-        // GET: Employee
-        //public async Task<ActionResult> Index()
-        //{
-        //    //var emps = await controller.Get() as OkNegotiatedContentResult<List<EmployeeDto>>;
-        //    //return View(emps);
-        //}
+        
+        //GET: Employee
+        public async Task<ActionResult> Index()
+        {
+            var emps = await controller.Get() as OkNegotiatedContentResult<List<EmployeeDto>>;
+            return View(emps.Content);
+        }
+
+      //  [Route("Employee/Details/{id}")]
+        public async Task<ActionResult> Details(string id)
+        {
+            var emp = await controller.Get(id) as OkNegotiatedContentResult<EmployeeDto>;
+            return View(emp.Content);
+        }
     }
 }
