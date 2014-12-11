e8fd52ab90b7cf0182491061b0b5dc8423b7b233
Christopher Blundell <christopher.n.blundell@gmail.com>
2014-12-11 14:35:10 +0100

    Added try-catch support for SaleListViewModel.

--- code/DriveIT/DriveIT.WindowsClient/ViewModels/SaleListViewModel.cs
+++ code/DriveIT/DriveIT.WindowsClient/ViewModels/SaleListViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Windows.Forms.VisualStyles;
 using DriveIT.Models;
 using DriveIT.WindowsClient.Controllers;
 using DriveIT.WindowsClient.Views;
@@ -51,45 +52,85 @@ namespace DriveIT.WindowsClient.ViewModels
         #region CRUD
         public async void ReadList()
         {
-            var saleController = new SaleController();
-            foreach (SaleDto saleDto in await saleController.ReadSaleList())
+            try
             {
-                SaleViewModels.Add(new SaleViewModel(saleDto));
+                var saleController = new SaleController();
+                foreach (SaleDto saleDto in await saleController.ReadSaleList())
+                {
+                    SaleViewModels.Add(new SaleViewModel(saleDto));
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw;
             }
         }
         public async void UpdateList()
         {
-            SaleViewModels.Clear();
-            var saleController = new SaleController();
-            foreach (SaleDto saleDto in await saleController.ReadSaleList())
+            try
             {
-                SaleViewModels.Add(new SaleViewModel(saleDto));
+                SaleViewModels.Clear();
+                var saleController = new SaleController();
+                foreach (SaleDto saleDto in await saleController.ReadSaleList())
+                {
+                    SaleViewModels.Add(new SaleViewModel(saleDto));
+                }
+            }
+            catch (Exception e)
+            {
+                
+                throw;
             }
         }
 
         public void DeleteSale()
         {
-            if (SelectedSale.SaleId.HasValue) SelectedSale.DeleteSale();
-            else
+            try
             {
-                SaleViewModels.Remove(SelectedSale);
-                SelectedSale = null;
+                if (SelectedSale.SaleId.HasValue) SelectedSale.DeleteSale();
+                else
+                {
+                    SaleViewModels.Remove(SelectedSale);
+                    SelectedSale = null;
+                }
+            }
+            catch (Exception e)
+            {
+                
+                throw;
             }
         }
 
         public void CreateNewSaleWindow()
         {
-            var newSale = new SaleViewModel();
-            var window = new EntitySaleWindow { DataContext = newSale };
-            SaleViewModels.Add(newSale);
-            window.Show();
+            try
+            {
+                var newSale = new SaleViewModel();
+                var window = new EntitySaleWindow { DataContext = newSale };
+                SaleViewModels.Add(newSale);
+                window.Show();
+            }
+            catch (Exception e)
+            {
+                
+                throw;
+            }
         }
 
         public void UpdateSaleWindow()
         {
-            SaleViewModel sale = SelectedSale;
-            var window = new EntitySaleWindow { DataContext = sale };
-            window.Show();
+            try
+            {
+                SaleViewModel sale = SelectedSale;
+                var window = new EntitySaleWindow { DataContext = sale };
+                window.Show();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
         }
         #endregion CRUD
     }
