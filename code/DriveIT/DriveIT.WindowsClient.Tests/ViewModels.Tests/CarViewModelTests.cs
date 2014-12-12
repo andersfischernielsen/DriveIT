7431c0e35aeea2e135863c84b8a027a8a665e974
Anders Wind Steffensen <awis@itu.dk>
2014-12-12 23:05:11 +0100

    Added two new testclasses and created some of the tests for passwordviewmodel

---
+++ code/DriveIT/DriveIT.WindowsClient.Tests/ViewModels.Tests/CarViewModelTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DriveIT.WindowsClient.Tests.ViewModels.Tests
+{
+    [TestFixture]
+    public class CarViewModelTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
