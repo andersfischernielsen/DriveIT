f1b07d97c4ec8f2ccb8a3080ede2a43cda9f029d
Anders Fischer-Nielsen <andersfischern@me.com>
2014-12-12 20:54:02 +0100

    Testing. I will kill someone soon. This is horrible.

--- code/DriveIT/DriveIT.EntityFramework.Tests/EntityStorageTests.cs
+++ code/DriveIT/DriveIT.EntityFramework.Tests/EntityStorageTests.cs
@@ -18,13 +18,16 @@ namespace DriveIT.Entities.Tests
     public class EntityStorageTests
     {
         private IPersistentStorage _toTest;
-        private Mock<DbSet<Car>> _mockSet;
+        private FakeDbSet<Car> _carMockSet;
+        private FakeDbSet<ImagePath> _imagePathMockSet;
         private Mock<DriveITContext> _mockContext;
 
         [TestFixtureSetUp]
         public void SetUp()
         {
             _toTest = new EntityStorage();
+
+            var imagePaths = new List<ImagePath> {new ImagePath {CarId = 1}, new ImagePath {CarId = 2}};
 
             var cars = new List<Car> 
             { 
@@ -41,7 +44,8 @@ namespace DriveIT.Entities.Tests
                     Price = 1000,
                     Sold = false,
                     Transmission = "Manual",
-                    Year = 2001
+                    Year = 2001, 
+                    ImagePaths = imagePaths
                 }, 
                 new Car
                 {
@@ -56,27 +60,52 @@ namespace DriveIT.Entities.Tests
                     Price = 20000,
                     Sold = true,
                     Transmission = "Manual",
-                    Year = 2001
+                    Year = 2001,
+                    ImagePaths = imagePaths
                 }, 
             }.AsQueryable();
 
-            _mockSet = new Mock<DbSet<Car>>();
-            _mockSet.As<IDbAsyncEnumerable<Car>>()
-                .Setup(m => m.GetAsyncEnumerator())
-                .Returns(new TestDbAsyncEnumerator<Car>(cars.GetEnumerator()));
+            var imagePath = imagePaths.ElementAt(0);
+            cars.ElementAt(0).ImagePaths.Add(imagePath);
+            cars.ElementAt(1).ImagePaths.Add(imagePath);
 
-            _mockSet.As<IQueryable<Car>>()
-                .Setup(m => m.Provider)
-                .Returns(new TestDbAsyncQueryProvider<Car>(cars.Provider));
+            _carMockSet = new FakeDbSet<Car>();
+            _carMockSet.SetData(cars);
 
-            _mockSet.As<IQueryable<Car>>().Setup(m => m.Expression).Returns(cars.Expression);
-            _mockSet.As<IQueryable<Car>>().Setup(m => m.ElementType).Returns(cars.ElementType);
-            _mockSet.As<IQueryable<Car>>().Setup(m => m.GetEnumerator()).Returns(cars.GetEnumerator());
+            _imagePathMockSet = new FakeDbSet<ImagePath>();
+            _imagePathMockSet.SetData(imagePaths);
 
-            _mockSet.Setup(m => m.FindAsync(It.IsAny<int>())).Returns(_mockSet.Object.FindAsync(1));
+            _carMockSet.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(_carMockSet.Object.FindAsync(1));
 
             _mockContext = new Mock<DriveITContext>();
-            _mockContext.Setup(c => c.Cars).Returns(_mockSet.Object);
+            _mockContext.Setup(c => c.Cars).Returns(_carMockSet.Object);
+            _mockContext.Setup(x => x.ImagePaths).Returns(_imagePathMockSet.Object);
+
+            _mockContext.Object.Cars.Include(p => p.ImagePaths);
+        }
+
+        internal class FakeDbSet<T> : Mock<DbSet<T>> where T : class
+        {
+            public void SetData(IEnumerable<T> data)
+            {
+                var mockDataQueryable = data.AsQueryable();
+
+                As<IQueryable<T>>().Setup(x => x.Provider).Returns(mockDataQueryable.Provider);
+                As<IQueryable<T>>().Setup(x => x.Expression).Returns(mockDataQueryable.Expression);
+                As<IQueryable<T>>().Setup(x => x.ElementType).Returns(mockDataQueryable.ElementType);
+                As<IQueryable<T>>().Setup(x => x.GetEnumerator()).Returns(mockDataQueryable.GetEnumerator());
+            }
+        }
+
+        private static Mock<IDbSet<T>> CreateMockSet<T>(IQueryable<T> childlessParents) where T : class
+        {
+            var mockSet = new Mock<IDbSet<T>>();
+
+            mockSet.Setup(m => m.Provider).Returns(childlessParents.Provider);
+            mockSet.Setup(m => m.Expression).Returns(childlessParents.Expression);
+            mockSet.Setup(m => m.ElementType).Returns(childlessParents.ElementType);
+            mockSet.Setup(m => m.GetEnumerator()).Returns(childlessParents.GetEnumerator());
+            return mockSet;
         }
 
         [Test]
@@ -99,18 +128,46 @@ namespace DriveIT.Entities.Tests
             },
             _mockContext.Object);
 
-            _mockSet.Verify(m => m.Add(It.IsAny<Car>()), Times.Once);
+            _carMockSet.Verify(m => m.Add(It.IsAny<Car>()), Times.Once);
             _mockContext.Verify(m => m.SaveChangesAsync());
         }
 
-        //[Test]
-        //public async void GetAllCarsTest()
-        //{
-        //    var result = await _toTest.GetAllCars(_mockContext.Object);
-        //    Assert.AreEqual(2, result.Count());
-        //    Assert.AreEqual("Ford", result[0].Make);
-        //    Assert.AreEqual("Bentley", result[1].Make);
-        //}
+        [Test]
+        public void Test()
+        {
+            var child = new ImagePath();
+            var parent = new Car { ImagePaths = new List<ImagePath>()};
+            parent.ImagePaths.Add(child);
+
+            var parents = new List<Car>
+                {
+                    parent
+                }.AsQueryable();
+
+            var children = new List<ImagePath>
+                {
+                    child
+                }.AsQueryable();
+
+            var mockContext = new Mock<DriveITContext>();
+
+            var mockParentSet = CreateMockSet(parents);
+            var mockChildSet = CreateMockSet(children);
+
+            mockContext.SetupGet(mc => (IDbSet<Car>) mc.Cars).Returns(mockParentSet.Object);
+            mockContext.SetupGet(mc => (IDbSet<ImagePath>) mc.ImagePaths).Returns(mockChildSet.Object);
+
+            var query = mockContext.Object.Cars.Include(p => p.ImagePaths).Select(p => p);
+        }
+
+        [Test]
+        public async void GetAllCarsTest()
+        {
+            var result = await _toTest.GetAllCars(_mockContext.Object);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("Ford", result[0].Make);
+            Assert.AreEqual("Bentley", result[1].Make);
+        }
 
         [Test]
         public async void DeleteCarTest()
@@ -118,7 +175,7 @@ namespace DriveIT.Entities.Tests
             await _toTest.DeleteCar(1, _mockContext.Object);
 
             Assert.IsNull(_mockContext.Object.Cars.FindAsync(1));
-            _mockSet.Verify(m => m.Remove(It.IsAny<Car>()), Times.Once);
+            _carMockSet.Verify(m => m.Remove(It.IsAny<Car>()), Times.Once);
             _mockContext.Verify(m => m.SaveChangesAsync(), Times.AtLeastOnce);
         }
 
