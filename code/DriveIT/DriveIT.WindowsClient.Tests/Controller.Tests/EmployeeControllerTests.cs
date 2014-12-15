0e7b4cdbbfea41b0a96f95f2ebb9aac5e00b396f
Anders Wind Steffensen <awis@itu.dk>
2014-12-16 00:30:16 +0100

    Updated the tests to reflect the new seed method

--- code/DriveIT/DriveIT.WindowsClient.Tests/Controller.Tests/EmployeeControllerTests.cs
+++ code/DriveIT/DriveIT.WindowsClient.Tests/Controller.Tests/EmployeeControllerTests.cs
@@ -16,7 +16,7 @@ namespace DriveIT.WindowsClient.Tests.Controller.Tests
         [TestFixtureSetUp]
         public void TestFixtureSetUp()
         {
-            DriveITWebAPI.Login("admin@driveIT.dk", "4dmin_Password").Wait();
+            DriveITWebAPI.Login("awis@itu.dk", "4dmin_Password").Wait();
             _employeeController = new EmployeeController();
             var empTask = _employeeController.CreateEmployee(new EmployeeDto
             {
