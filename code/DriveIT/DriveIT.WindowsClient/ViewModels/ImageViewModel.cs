4140fca77a543e7ffcf13758f8a7f3581ce198d4
Anders Wind Steffensen <awis@itu.dk>
2014-12-15 10:52:48 +0100

    Documented the last classes

--- code/DriveIT/DriveIT.WindowsClient/ViewModels/ImageViewModel.cs
+++ code/DriveIT/DriveIT.WindowsClient/ViewModels/ImageViewModel.cs
@@ -10,6 +10,9 @@ using Microsoft.Win32;
 
 namespace DriveIT.WindowsClient.ViewModels
 {
+    /// <summary>
+    /// The ImageViewmodel represents the filepath/url to an image, and the functionality to choose an image on your pc.
+    /// </summary>
     public class ImageViewModel : IViewModelBase
     {
         #region INotifyPropertyChanged
@@ -24,10 +27,17 @@ namespace DriveIT.WindowsClient.ViewModels
         }
         #endregion
 
+        /// <summary>
+        /// Empty constructer
+        /// </summary>
         public ImageViewModel()
         {
         }
 
+        /// <summary>
+        /// Constructor taken a string which should represent a filepath/url to an image.
+        /// </summary>
+        /// <param name="imagePath"></param>
         public ImageViewModel(string imagePath)
         {
             ImagePath = imagePath;
@@ -47,12 +57,15 @@ namespace DriveIT.WindowsClient.ViewModels
         }
 
         #endregion Properties
+        /// <summary>
+        /// Creates a openfiledialog and waits for the user to either pick an image or close it.
+        /// </summary>
         public void ChooseFile()
         {
             try
             {
                 OpenFileDialog openFileDialog = new OpenFileDialog();
-                openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg;*.gif)|*.png;*.jpeg;*.jpg;*.gif|All files (*.*)|*.*";
+                openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg;*.gif)|*.png;*.jpeg;*.jpg;*.gif";
                 if (openFileDialog.ShowDialog() == true)
                 {
                     ImagePath = openFileDialog.FileName;
@@ -65,6 +78,10 @@ namespace DriveIT.WindowsClient.ViewModels
             }
         }
 
+        /// <summary>
+        /// Checks if the imagepath is empty or whitespace.
+        /// </summary>
+        /// <returns></returns>
         public bool IsEmpty()
         {
             if (string.IsNullOrWhiteSpace(ImagePath)) return true;
